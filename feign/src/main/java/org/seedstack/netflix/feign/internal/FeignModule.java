c40ec2df211d0fb3730b4a1398b4eacfe41fd47d
adriendomurado <adrien.domurado@gmail.com>
2016-10-12 12:08:38 +0200

    creation of module netflix-feign

--- feign/src/main/java/org/seedstack/netflix/feign/internal/FeignModule.java
+++ feign/src/main/java/org/seedstack/netflix/feign/internal/FeignModule.java
@@ -1,17 +1,17 @@
 /**
  * Copyright (c) 2013-2016, The SeedStack authors <http://seedstack.org>
- *
+ * <p>
  * This Source Code Form is subject to the terms of the Mozilla Public
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/.
  */
-package org.seedstack.netflix.hystrix.internal;
+package org.seedstack.netflix.feign.internal;
 
-import org.seedstack.seed.core.internal.AbstractSeedPlugin;
+import org.seedstack.seed.core.internal.AbstractSeedModule;
 
-public class HystrixPlugin extends AbstractSeedPlugin {
+public class FeignModule extends AbstractSeedModule{
     @Override
-    public String name() {
-        return "netflix-hystrix";
+    protected void configure() {
+
     }
 }
