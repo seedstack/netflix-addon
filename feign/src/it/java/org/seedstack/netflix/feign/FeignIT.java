5e204181b9ff9ab9274734fc38425961c6cd455b
adriendomurado <adrien.domurado@gmail.com>
2016-10-24 14:15:58 +0200

    Fix tests on Arquillian

--- feign/src/it/java/org/seedstack/netflix/feign/FeignIT.java
+++ feign/src/it/java/org/seedstack/netflix/feign/FeignIT.java
@@ -7,21 +7,20 @@
  */
 package org.seedstack.netflix.feign;
 
+import feign.Feign;
+import feign.jackson.JacksonDecoder;
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.junit.Before;
 import org.junit.Test;
 import org.seedstack.netflix.feign.fixtures.Message;
 import org.seedstack.netflix.feign.fixtures.TestAPI;
 import org.seedstack.netflix.feign.fixtures.TestResource;
-import org.seedstack.netflix.feign.internal.FeignConfig;
-import org.seedstack.seed.Configuration;
+import org.seedstack.netflix.feign.internal.FeignApi;
 import org.seedstack.seed.it.AbstractSeedWebIT;
 
 import javax.inject.Inject;
-
 import java.net.URL;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -41,14 +40,16 @@ public class FeignIT extends AbstractSeedWebIT {
 
     @Test
     public void test_API() {
-        assertThat(testAPI).isNotNull();
-        Message message = testAPI.getMessage();
+        // Cannot use injected api because Arquillian server starts on a random url each time
+        TestAPI api = Feign.builder().decoder(new JacksonDecoder()).target(TestAPI.class, baseUrl.toExternalForm());
+        Message message = api.getMessage();
         assertThat(message.getBody()).isEqualTo("Hello World !");
         assertThat(message.getAuthor()).isEqualTo("computer");
     }
 
     @Test
     public void test_fallback() {
+        assertThat(testAPI).isNotNull();
         Message message = testAPI.get404();
         assertThat(message.getBody()).isEqualTo("Error code: 404 !");
         assertThat(message.getAuthor()).isEqualTo("fallback");
