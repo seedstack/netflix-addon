d19d8b50ef5378871370268b8763da8af76f4260
adriendomurado <adrien.domurado@gmail.com>
2017-01-27 14:37:53 +0100

    Test service registration

--- eureka/src/it/java/org/seedstack/netflix/eureka/EurekaIT.java
+++ eureka/src/it/java/org/seedstack/netflix/eureka/EurekaIT.java
@@ -13,12 +13,6 @@ import org.junit.Test;
 import org.seedstack.seed.it.AbstractSeedIT;
 
 import javax.inject.Inject;
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.io.PrintStream;
-import java.net.InetSocketAddress;
-import java.net.Socket;
-import java.util.Date;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -28,39 +22,22 @@ public class EurekaIT extends AbstractSeedIT {
     private EurekaClient eurekaClient;
 
     @Test
-    public void eurekaClientIsRunningCorrectly() throws Exception {
+    public void eurekaIsInjected() throws Exception {
         assertThat(eurekaClient).isNotNull();
+    }
 
-        // initialize the client
-        // this is the vip address for the example service to talk to as defined in conf/sample-eureka-service.properties
+    @Test
+    public void eurekaRegisterToTheServer() throws Exception {
         String vipAddress = "sampleservice.mydomain.net";
-
         InstanceInfo nextServerInfo = null;
-        nextServerInfo = eurekaClient.getNextServerFromEureka(vipAddress, false);
-
-        System.out.println("Found an instance of example service to talk to from eureka: "
-                + nextServerInfo.getVIPAddress() + ":" + nextServerInfo.getPort());
-
-        System.out.println("healthCheckUrl: " + nextServerInfo.getHealthCheckUrl());
-        System.out.println("override: " + nextServerInfo.getOverriddenStatus());
-
-        Socket s = new Socket();
-        int serverPort = nextServerInfo.getPort();
-        s.connect(new InetSocketAddress(nextServerInfo.getHostName(), serverPort));
-        String request = "FOO " + new Date();
-        System.out.println("Connected to server. Sending a sample request: " + request);
-
-        PrintStream out = new PrintStream(s.getOutputStream());
-        out.println(request);
-
-        System.out.println("Waiting for server response..");
-        BufferedReader rd = new BufferedReader(new InputStreamReader(s.getInputStream()));
-        String str = rd.readLine();
-        if (str != null) {
-            System.out.println("Received response from server: " + str);
-            assertThat(str).startsWith("BAR");
-            System.out.println("Exiting the client. Demo over..");
+        while (nextServerInfo == null) {
+            try {
+                nextServerInfo = eurekaClient.getNextServerFromEureka(vipAddress, false);
+            } catch (Throwable e) {
+                System.out.println("Waiting ... verifying service registration with eureka ...");
+                Thread.sleep(10_000);
+            }
         }
-        rd.close();
+        assertThat(nextServerInfo.getAppName()).isEqualTo("SAMPLEREGISTERINGSERVICE");
     }
 }
